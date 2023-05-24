5a3321d970d5f2ecdb899430558dcdf5db01baea
Anjali Arya <anjalisinha498@gmail.com>
2023-05-24 21:36:10 +0530

    added validations for register form

--- src/app/components/Form.js
+++ src/app/components/Form.js
@@ -1,68 +1,76 @@
 "use client";
 import {useFormik} from "formik";
 import Input from './input/Input';
-import TelInput from "./input/tel_input";
+import  { signUpSchema } from "./../schemas";
+// import TelInput from "./input/tel_input";
 
 const initialValues = {
+    name: "",
     email: "",
     password: "",
     repeat_password: "",
-    first_name: "",
-    last_name: "",
-    phone: "",
-    company: "",
-
 }
 
 
-const Form = () => {
+const Registration = () => {
 
 
-    const Formik = useFormik({
-        initialValues: initialValues,
-        onSubmit: (values) => {
-            console.log(values); 
-        }
+    const { values, errors, touched, handleBlur, handleChange, handleSubmit } =
+    useFormik({
+      initialValues,
+      validationSchema: signUpSchema,
+      onSubmit: (values, action) => {
+        console.log(
+          "🚀 ~ file: Registration.jsx ~ line 11 ~ Registration ~ values",
+          values
+        );
+        action.resetForm();
+      },
     });
-
-    console.log(Formik)
-
+  console.log(
+    "🚀 ~ file: Registration.jsx ~ line 25 ~ Registration ~ errors",
+    errors
+    
+  );
 
     return (
         <section className='w-8/12 md:w-6/12 mx-auto py-20 my-56 h-full self-stretch '>
             
-            <form className='p-10 rounded-md shadow-lg shadow-gray-900'> 
+            <form className='p-10 rounded-md shadow-lg shadow-gray-900' onSubmit={handleSubmit}> 
+
                 <div className="relative z-0 mb-6 group">
-                    <Input label={"Enter Email:"} id={"email"} type={"email"} help_text={"a@gmail.com"} />
+                    <Input LabelName={"Enter Full Name:"} id={"name"} type={"text"} value={values.name} onChange={handleChange} onBlur={handleBlur} />
+                    
+                    {errors.name && touched.name ? (
+                        <p className="form-error">{errors.name}</p>
+                      ) : null}
+
+                </div>
+                <div className="relative z-0 mb-6 group">
+                    <Input LabelName={"Enter Email:"} id={"email"} type={"email"} value={values.email} onChange={handleChange} onBlur={handleBlur} />
+                    
+                    {errors.email && touched.email ? (
+                        <p className="form-error">{errors.email}</p>
+                      ) : null}
+
                 </div>
                 <div className="relative z-0 w-full mb-6 group">
-                    <Input label={"Enter Password:"} id={"password"} type={"password"} />
+                    <Input LabelName={"Enter Password:"} id={"password"} type={"password"} value={values.password} onChange={handleChange} onBlur={handleBlur}/>
+                    
+                    {errors.password && touched.password ? (
+                        <p className="form-error">{errors.password}</p>
+                      ) : null}
 
                 </div>
                 <div className="relative z-0 w-full mb-6 group">
-                    <Input label={"Repeat Password:"} id={"rep_password"} type={"password"} />
+                    <Input LabelName={"Repeat Password:"} id={"rep_password"} type={"password"} value={values.repeat_password} onChange={handleChange} onBlur={handleBlur}/>
+                    
+                    {errors.repeat_password && touched.repeat_password ? (
+                        <p className="form-error">{errors.repeat_password}</p>
+                      ) : null}
 
                 </div>
-                <div className="grid md:grid-cols-2 md:gap-6">
-                    <div className="relative z-0 w-full mb-6 group">
-                        <Input label={"Enter First Name:"} id={"first_name"} type={"text"} help_text={"Anjali"} />
 
-                    </div>
-                    <div className="relative z-0 w-full mb-6 group">
-                    <Input label={"Enter Last Name:"} id={"last_name"} type={"text"} help_text={"Arya"} />
-
-                    </div>
-                </div>
-                <div className="grid md:grid-cols-2 md:gap-6">
-                    <div className="relative z-0 w-full mb-6 group">
-                        <TelInput/>
-
-                    </div>
-                    <div className="relative z-0 w-full mb-6 group">
-                        <Input label={"Enter Company Name:"} id={"company"} type={"text"} help_text={"Ex. Google"} />
-
-                    </div>
-                </div>
             <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
             </form>
 
@@ -71,4 +79,4 @@ const Form = () => {
     );
 }
 
-export default Form;
+export default Registration;
